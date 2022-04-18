04c4b01809fb04ffafec526d95ff6b8b47b07ba6
ssj <760797012@qq.com>
2022-04-18 21:26:24 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -4,5 +4,25 @@
 $.ajaxPrefilter(function (options) {
     // 在发起真正的ajax请求之前，统一拼接请求的根路径
     options.url = 'http://www.liulongbin.top:3007' + options.url;
-    console.log(options.url);
+    // 统一为有权限的接口，设置headers 请求头
+    if (options.url.indexOf('/my') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+
+        // 统一挂载complete回调函数
+        // 无论成功还是失败，最终都会调用complete回调函数
+        options.complete = function (res) { 
+            // console.log('调用了complete函数');
+            // console.log(res);
+            // 在complete 回调函数中，可以使用 res.responseJSON 拿到服务器
+            // 响应回来的数据
+            if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+                // 1.强制清空 token
+                localStorage.removeItem('token');
+                // 2.强制跳转到登录页面
+                location.href = 'login.html'; 
+             }
+        }
+    }
 })
--- assets/js/login.js
+++ assets/js/login.js
@@ -67,7 +67,7 @@ $(function () {
                 // 将登录成功的得到的 token字符串，保存到localStorage中
                 localStorage.setItem('token',res.token);
                 // 跳转到后台主页
-                location.href = '/index.html';
+                location.href = 'index.html';
                 console.log(res);
                 
             }
